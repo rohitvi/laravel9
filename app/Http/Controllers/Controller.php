f01e4625663fee2288e7586d1f82f7e5c3468600
rohitvi <rohit.vish@darwinpgc.com>
2022-09-13 11:04:47 +0000

    docker

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,6 +11,6 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function show(){
-        return "Rohit accessing controller";
+        return "Rohit accessing from docker";
     }
 }
